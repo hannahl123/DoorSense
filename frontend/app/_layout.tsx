0e6e6bf302f4858b8d6da5d169fbce50abf7243b
Hannah <53445855+hannahl123@users.noreply.github.com>
2024-11-05 21:19:55 -0500

    Created nav bar
    - Created nav bar
    - Created 3 pages
    - Updated colour theme
    - Finished nav bar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,70 @@
-import { Stack } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Colors } from "../constants/Colors";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        headerShown: false, 
+        tabBarStyle: {
+          height: '12%', 
+          paddingTop: '3%', 
+          marginBottom: '3%',
+          marginHorizontal: '5%', 
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          paddingTop: '3%',
+          fontSize: 13, 
+          fontWeight: 500,
+        }, 
+        tabBarIconStyle: {
+          paddingBottom: '3%',
+        },
+        tabBarActiveTintColor: Colors.light.background, 
+        tabBarInactiveTintColor: Colors.light.text, 
+      }}
+    >
+      <Tabs.Screen
+        name="index" 
+        options={{
+          title: 'HOME',
+          tabBarIcon: ({focused}) => (
+            <MaterialIcons 
+              name={'home'} 
+              color={focused ? '#52B69A' : '#1E6091'} 
+              size={35}
+            />
+          ), 
+        }}
+      />
+      <Tabs.Screen
+        name="recent" 
+        options={{
+          title: 'RECENT',
+          tabBarIcon: ({focused}) => (
+            <MaterialIcons 
+              name={'schedule'} 
+              color={focused ? '#52B69A' : '#1E6091'} 
+              size={35}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="settings" 
+        options={{
+          title: "SETTINGS", 
+          tabBarIcon: ({focused}) => (
+            <MaterialIcons 
+              name={'settings'} 
+              color={focused ? '#52B69A' : '#1E6091'} 
+              size={35}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#1E6091';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#1E6091',
+    background: '#52B69A',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#1E6091',
+    tabIconDefault: '#1E6091',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#fff',
+    background: '#1E6091',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: '#1E6091',
+    tabIconDefault: '#1E6091',
     tabIconSelected: tintColorDark,
   },
 };
