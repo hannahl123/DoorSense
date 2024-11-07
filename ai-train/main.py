65377ef2c3e8bf1aa9b9105148060f0c97926d9b
Davis Liu <davisliu2006@gmail.com>
2024-11-06 21:53:36 -0500

    AI train

--- ai-train/main.py
+++ ai-train/main.py
@@ -1,1 +1,22 @@
-import tflite_model_maker as tfmm
+import keras
+import tensorflow as tf
+
+#model
+model = keras.Sequential([
+    keras.layers.InputLayer(shape=(28,28)),
+    keras.layers.Dense(32, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+)
+model.fit(train_images, train_labels, epochs=5);
+
+# convert
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+with tf.io.gfile.GFile("model.tflite", "wb") as f:
+    f.write(tflite_model)
