f0bd2a7fa8a76753e6ac4ad6d1c90bffe820fc65
Davis Liu <davisliu2006@gmail.com>
2024-11-07 23:22:46 -0500

    Attempt setup basic file structure

--- ai-run/main.py
+++ ai-run/main.py
@@ -1,1 +1,11 @@
-import tflite_support as tf
+# setup
+import pathlib
+def is_raspPI(): return pathlib.Path("/etc/rpi-issue").exists()
+print(is_raspPI())
+
+if is_raspPI():
+    import tflite_runtime.interpreter as tfl_interpreter
+else:
+    import tensorflow.lite.python.interpreter as tfl_interpreter
+
+interpreter = tfl_interpreter.Interpreter(model_path="");
--- ai-train/main.py
+++ ai-train/main.py
@@ -1,13 +1,36 @@
 import keras
 import tensorflow as tf
 
-#model
+# model
 model = keras.Sequential([
     keras.layers.InputLayer(shape=(28,28)),
     keras.layers.Dense(32, activation="relu"),
     keras.layers.Dense(10, activation="softmax")
 ])
 
+# load data
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "data",
+    image_size=(28, 28), # resize images
+    color_mode="rgb",
+    batch_size=32,
+    label_mode="int" # "labels will be integer indices based on class folder names"
+)
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "data",
+    image_size=(28, 28),
+    color_mode="rgb",
+    batch_size=32,
+    label_mode="int"
+)
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32) / 255.0
+    return images, labels
+
+train_dataset = train_dataset.map(normalize)
+test_dataset = test_dataset.map(normalize)
+
+# train
 model.compile(
     optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
 )
@@ -18,5 +41,6 @@ converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
+# write
 with tf.io.gfile.GFile("model.tflite", "wb") as f:
     f.write(tflite_model)
