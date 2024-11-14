5b018e03cf75122c12d987d3cd98c2071f7d1587
Davis Liu <davisliu2006@gmail.com>
2024-11-13 23:52:28 -0500

    Setup ai-train and ai-run structure

--- ai-run/main.py
+++ ai-run/main.py
@@ -1,11 +1,27 @@
-# setup
+import os
 import pathlib
-def is_raspPI(): return pathlib.Path("/etc/rpi-issue").exists()
-print(is_raspPI())
 
+# setup
+def is_raspPI(): return pathlib.Path("/etc/rpi-issue").exists()
+print("Is Raspberry PI: "+str(is_raspPI()))
 if is_raspPI():
     import tflite_runtime.interpreter as tfl_interpreter
 else:
-    import tensorflow.lite.python.interpreter as tfl_interpreter
+    import numpy as np
+    import tensorflow as tf
 
-interpreter = tfl_interpreter.Interpreter(model_path="");
+dir = os.path.dirname(__file__)
+interpreter = tf.lite.Interpreter(dir+"/model/parcel_detection.lite")
+interpreter.allocate_tensors()
+
+# get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# test the model on random input data
+input_shape = input_details[0]["shape"]
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.int8)
+interpreter.set_tensor(input_details[0]["index"], input_data)
+
+# invoke interpreter
+interpreter.invoke()
--- ai-train/main.py
+++ ai-train/main.py
@@ -1,40 +1,39 @@
+import os
 import keras
 import tensorflow as tf
 
+VIDEO_X = 640
+VIDEO_Y = 360
+
 # model
 model = keras.Sequential([
-    keras.layers.InputLayer(shape=(28,28)),
+    keras.layers.InputLayer(shape=(VIDEO_X, VIDEO_Y)),
     keras.layers.Dense(32, activation="relu"),
     keras.layers.Dense(10, activation="softmax")
 ])
 
 # load data
-train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    "data",
-    image_size=(28, 28), # resize images
+dir = os.path.dirname(__file__)
+train_dataset = keras.preprocessing.image_dataset_from_directory(
+    dir+"/data",
+    image_size=(VIDEO_X, VIDEO_Y), # resize images
     color_mode="rgb",
     batch_size=32,
     label_mode="int" # "labels will be integer indices based on class folder names"
 )
-test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    "data",
-    image_size=(28, 28),
-    color_mode="rgb",
-    batch_size=32,
-    label_mode="int"
-)
-def normalize(images, labels):
-    images = tf.cast(images, tf.float32) / 255.0
-    return images, labels
-
-train_dataset = train_dataset.map(normalize)
-test_dataset = test_dataset.map(normalize)
+# test_dataset = keras.preprocessing.image_dataset_from_directory(
+#     dir+"/data",
+#     image_size=(VIDEO_X, VIDEO_Y),
+#     color_mode="rgb",
+#     batch_size=32,
+#     label_mode="int"
+# )
 
 # train
 model.compile(
     optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
 )
-model.fit(train_images, train_labels, epochs=5);
+model.fit(train_dataset, epochs=5);
 
 # convert
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
