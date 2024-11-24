0d1e5ba9a9d526c032f3ff69ad128d51b7dbc463
Hannah <53445855+hannahl123@users.noreply.github.com>
2024-11-23 22:03:31 -0500

    Changed dark mode active colours

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -23,8 +23,8 @@ export default function TabsLayout() {
           fontSize: 13,
           fontWeight: 500,
         },
-        tabBarActiveTintColor: colors.secondary,
-        tabBarInactiveTintColor: colors.primary,
+        tabBarActiveTintColor: colors.selected,
+        tabBarInactiveTintColor: colors.unselected,
       }}
     >
       <Tabs.Screen
@@ -34,7 +34,7 @@ export default function TabsLayout() {
           tabBarIcon: ({ focused }) => (
             <MaterialIcons
               name={"home"}
-              color={focused ? colors.secondary : colors.primary}
+              color={focused ? colors.selected : colors.unselected}
               size={30}
             />
           ),
@@ -47,7 +47,7 @@ export default function TabsLayout() {
           tabBarIcon: ({ focused }) => (
             <MaterialIcons
               name={"schedule"}
-              color={focused ? colors.secondary : colors.primary}
+              color={focused ? colors.selected : colors.unselected}
               size={30}
             />
           ),
@@ -60,7 +60,7 @@ export default function TabsLayout() {
           tabBarIcon: ({ focused }) => (
             <MaterialIcons
               name={"settings"}
-              color={focused ? colors.secondary : colors.primary}
+              color={focused ? colors.selected : colors.unselected}
               size={30}
             />
           ),
--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -8,12 +8,16 @@ export const Colors = {
     secondary: '#52B69A', 
     background: '#FFF', 
     icon: '#1E6091', 
+    selected: '#52B69A', 
+    unselected: '#1E6091', 
   },
   dark: {
     primary: '#FFF', 
     secondary: '#52B69A', 
     background: '#1E6091', 
     icon: '#1E6091', 
+    selected: '#FFF', 
+    unselected: '#52B69A', 
   },
 };
 
