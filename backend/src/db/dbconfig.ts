98cefeee1f706bdffc71dddbbe61e4a2e79baeba
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-11-23 22:41:43 -0500

    Read DB password from .env file

--- backend/src/db/dbconfig.ts
+++ backend/src/db/dbconfig.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Doorsense',
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "Doorsense",
     connectionLimit: 5,
-}
+};
 
 export default config;
