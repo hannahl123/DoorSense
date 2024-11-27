78a06a634712eda60d7bcb1eb02d3da6bc2e4ee3
Bianca Bai <biancabaixiuqi@outlook.com>
2024-11-26 21:26:18 -0500

    Solving merge conflicts

---
+++ backend/src/db/dbconfig.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(`DB Password: ${process.env.DB_PASSWORD}`);
+
+const config = {
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "doorsense",
+    connectionLimit: 5,
+};
+
+export default config;
