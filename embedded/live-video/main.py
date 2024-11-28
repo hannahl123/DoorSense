c2264df3823b8ced3b8b82f8f40a328650b82814
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-11-28 18:03:08 -0500

    Resize video

--- embedded/live-video/main.py
+++ embedded/live-video/main.py
@@ -13,9 +13,9 @@ class RPiVideoStream:
 
         self.camera = cv2.VideoCapture(0)
 
-        self.width, self.height = 640, 480
-        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.width, self.height = 315, 230
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 
         self.sio = socketio.Client()
         self.server_url = server_url
@@ -24,7 +24,8 @@ class RPiVideoStream:
 
     def _connect_to_server(self):
         try:
-            self.sio.connect(self.server_url)
+            if not self.sio.connected:
+                self.sio.connect(self.server_url)
             print(f"Raspberry Pi connected to server at {self.server_url}")
         except Exception as e:
             print(f"Error while connecting to server at {self.server_url}: {e}")
@@ -48,7 +49,7 @@ class RPiVideoStream:
             frame_base64 = base64.b64encode(buffer).decode("utf-8")
 
             # send frame via web socket
-            self.sio.emit("video_stream", {"frame": frame_base64})
+            self.sio.emit("incoming_video_stream", {"frame": frame_base64})
 
             time.sleep(0.03)  # ~30 frames/second
 
@@ -59,5 +60,5 @@ class RPiVideoStream:
 
 
 if __name__ == "__main__":
-    streamer = RPiVideoStream()
+    streamer = RPiVideoStream("http://localhost:8080")
     streamer.stream_video()
